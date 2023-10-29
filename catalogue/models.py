67eb5b0ce1c568c8f7f386867f72c6b9034685d6
Sean Johnson <98856218+sjohns2020@users.noreply.github.com>
2023-10-29 15:25:06 +0000

    added postgres database and seed data and build book and author models

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, related_name="books", on_delete=models.CASCADE)
+    publication_year = models.IntegerField()
+    ISBN = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.title
--- spooky_books/settings.py
+++ spooky_books/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "catalogue",
+    "information",
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = "spooky_books.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "spooky_books",
+        "USER": "codeclanstudent",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
